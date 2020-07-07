f7c6bf6bc57a38712c50128df7f00e88c54332e6
albert <amuluowin@163.com>
2020-07-07 10:17:15 +0800

    remove dep

--- src/Aop.php
+++ src/Aop.php
@@ -4,8 +4,6 @@
 namespace rabbit\aop;
 
 use Go\Instrument\Transformer\StreamMetaData;
-use rabbit\helper\ClassHelper;
-use rabbit\helper\FileHelper;
 
 /**
  * Class Aop
@@ -43,7 +41,7 @@ class Aop
                     if ($item->getIncludePath()) {
                         foreach ($item->getEnumerator()->enumerate() as $file) {
                             $contents = file_get_contents($file);
-                            $class = ClassHelper::getClassByString($contents);
+                            $class = $this->getClassByString($contents);
                             if (!empty($class)) {
                                 $aopFile = $item->findFile($class);
                                 if (strpos($aopFile, 'php://') === 0) {
@@ -55,10 +53,10 @@ class Aop
                                     fclose($fp);
                                     SourceTransformingLoader::transformCode($metadata);
                                     $context = $metadata->source;
-                                    $aopClass = ClassHelper::getClassByString($context);
+                                    $aopClass = $this->getClassByString($context);
                                     if (strpos($aopClass, '__AopProxied') !== false) {
                                         $dir = $cacheDir . '/' . $file->getPathname();
-                                        FileHelper::createDirectory(dirname($dir), 0777);
+                                        $this->createDirectory(dirname($dir), 0777);
                                         $len = file_put_contents(
                                             $dir,
                                             $context
@@ -73,6 +71,104 @@ class Aop
                     }
                 }
             }
+        }
+    }
+
+    /**
+     * @param string $contents
+     * @return string
+     */
+    public function getClassByString(string $contents): string
+    {
+        //Start with a blank namespace and class
+        $namespace = $class = "";
+
+        //Set helper values to know that we have found the namespace/class token and need to collect the string values after them
+        $getting_namespace = $getting_class = false;
+
+        //Go through each token and evaluate it as necessary
+        foreach (token_get_all($contents) as $token) {
+
+            //If this token is the namespace declaring, then flag that the next tokens will be the namespace name
+            if (is_array($token) && $token[0] == T_NAMESPACE) {
+                $getting_namespace = true;
+            }
+
+            //If this token is the class declaring, then flag that the next tokens will be the class name
+            if (is_array($token) && $token[0] == T_CLASS) {
+                $getting_class = true;
+            }
+
+            //While we're grabbing the namespace name...
+            if ($getting_namespace === true) {
+
+                //If the token is a string or the namespace separator...
+                if (is_array($token) && in_array($token[0], [T_STRING, T_NS_SEPARATOR])) {
+
+                    //Append the token's value to the name of the namespace
+                    $namespace .= $token[1];
+                } else {
+                    if ($token === ';') {
+
+                        //If the token is the semicolon, then we're done with the namespace declaration
+                        $getting_namespace = false;
+                    }
+                }
+            }
+
+            //While we're grabbing the class name...
+            if ($getting_class === true) {
+
+                //If the token is a string, it's the name of the class
+                if (is_array($token) && $token[0] == T_STRING) {
+
+                    //Store the token's value as the class name
+                    $class = $token[1];
+
+                    //Got what we need, stope here
+                    break;
+                }
+            }
+        }
+
+        //Build the fully-qualified class name and return it
+        return $namespace ? $namespace . '\\' . $class : $class;
+    }
+
+    /**
+     * @param string $path
+     * @param int $mode
+     * @param bool $recursive
+     * @return bool
+     * @throws \Exception
+     */
+    public function createDirectory(string $path, int $mode = 0775, bool $recursive = true): bool
+    {
+        if (is_dir($path)) {
+            return true;
+        }
+        $parentDir = dirname($path);
+        // recurse if parent dir does not exist and we are not at the root of the file system.
+        if ($recursive && !is_dir($parentDir) && $parentDir !== $path) {
+            static::createDirectory($parentDir, $mode, true);
+        }
+        try {
+            if (!mkdir($path, $mode)) {
+                return false;
+            }
+        } catch (\Exception $e) {
+            if (!is_dir($path)) {
+                throw new \Exception("Failed to create directory \"$path\": " . $e->getMessage(), $e->getCode(), $e);
+            }
+        }
+        try {
+            return chmod($path, $mode);
+        } catch (\Exception $e) {
+            throw new \Exception(
+                "Failed to change permissions for directory \"$path\": " . $e->getMessage(),
+                $e->getCode(),
+                $e
+            );
         }
     }
 }
